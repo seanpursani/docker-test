6bedb90ee6d258c37b2b777bafb5d06b29bf41f0
Sean Pursani <seanpursani@gmail.com>
2022-03-30 16:31:38 +0200

    added

---
+++ src/test/java/com/example/seandocker/SeanDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seandocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeanDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
